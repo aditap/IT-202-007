3dd4aaac19df2408806bdcdace234e41c17f44f7
aditap <31078704+aditap@users.noreply.github.com>
2019-11-01 22:08:04 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$host="sql1.njit.edu";//your msql host [sql1, sql2, sql3]
+$database="aap252";//ucid
+$username="aap252";//ucid
+$password="Ap02503098";//mysql password (not ucid password)
+?>
